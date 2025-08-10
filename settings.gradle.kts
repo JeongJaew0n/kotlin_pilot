6c26f981c58e46dd186c4122b4e6f2ec911eade9
HeLLo2 <popt0@naver.com>
2025-08-10 19:11:56 +0900

    lecture 삭제
    강사가 지혼자만 떠듬

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,1 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-rootProject.name = "ktor_lecture_code"
-
+rootProject.name = "kotlin-with-java"
--- src/main/kotlin/common/validation/Route.kt
+++
@@ -1,4 +0,0 @@
-package org.ktor_lecture.common.validation
-
-
-// post, get
