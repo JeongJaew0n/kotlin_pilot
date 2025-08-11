248bfbf839e9e5e31d4c949ce3439a0d08ba41ca
HeLLo2 <popt0@naver.com>
2025-08-12 01:02:17 +0900

    Update for.kt

--- src/main/kotlin/secton3/for.kt
+++ src/main/kotlin/secton3/for.kt
@@ -1,9 +1,25 @@
 package secton3
 
+// .. 연산자가 등차수열을 만들어줌.
+// while문은 jav와 동일함.
+// downTo, step 도 함수임.
+
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5)
 
     for( number in numbers) {
+        println("Number: $number")
+    }
+
+    for (number in 1..5) {
+        println("Number: $number")
+    }
+
+    for (number in 1..5 step 2) {
+        println("Number: $number")
+    }
+
+    for (number in  5 downTo  1) {
         println("Number: $number")
     }
 
